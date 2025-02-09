52c0e0b0064ac75df9f6ab4bea6d3c19988b9d78
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-02-09 20:59:58 +0530

    System_Monitoring

--- Assignment3/System_Monitoring/pr_3.sh
+++ Assignment3/System_Monitoring/pr_3.sh
@@ -2,3 +2,10 @@ sudo sysctl -w fs.file-max=100000
 sudo sysctl -w vm.swappiness=10
 sudo sysctl -w fs.file-max=100000
 sudo sysctl -w net.ipv4.ip_forward=1
+
+# Khushi@KhushiLaptop MINGW64 /d/OScript/Assignment3/System_Monitoring (Submission)
+# $ bash pr_3.sh
+# fs.file-max = 100000
+# vm.swappiness = 10
+# net.ipv4.ip_forward = 1
+
