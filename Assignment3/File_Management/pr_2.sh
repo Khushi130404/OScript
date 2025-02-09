8462022a35ad6c1d2d770fbdba14a4b3c4779f4f
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-02-09 20:37:24 +0530

    File_Management

--- Assignment3/File_Management/pr_2.sh
+++ Assignment3/File_Management/pr_2.sh
@@ -1,3 +1,28 @@
 # Search for files modified within the last 7 days using find.
 
 find D:/ -type f -mtime -7 -exec ls -lh {} \;
+
+# Khushi@KhushiLaptop MINGW64 /d/OScript/Assignment3/File_Management (Submission)
+# $ bash pr_2.sh
+# -rw-r--r-- 1 Khushi 197121 193K Feb  8 15:51 D:/DL_Amit_Sir/Digit_Detection/digit_detection.ipynb
+# -rw-r--r-- 1 Khushi 197121 879K Feb  7 18:55 D:/DL_Amit_Sir/Digit_Neural_Network/dnn1.ipynb
+# -rw-r--r-- 1 Khushi 197121 47K Feb  7 19:04 D:/DL_Amit_Sir/Digit_Neural_Network/dnn2.ipynb
+# -rw-r--r-- 1 Khushi 197121 1.4K Feb  8 15:15 D:/DL_Amit_Sir/NeuralNetwork.txt
+# -rw-r--r-- 1 Khushi 197121 775 Feb  7 18:48 D:/DL_Amit_Sir/Perceptron.txt
+# -rw-r--r-- 1 Khushi 197121 887K Feb  7 18:19 D:/DL_Amit_Sir/TensorFlow_1/first.ipynb
+# -rw-r--r-- 1 Khushi 197121 19 Feb  9 00:13 D:/MediReact/.git/COMMIT_EDITMSG
+# -rw-r--r-- 1 Khushi 197121 1.3K Feb  9 00:17 D:/MediReact/.git/FETCH_HEAD
+# -rw-r--r-- 1 Khushi 197121 26 Feb  8 22:28 D:/MediReact/.git/HEAD
+# -rw-r--r-- 1 Khushi 197121 9.6K Feb  9 00:13 D:/MediReact/.git/index
+# -rw-r--r-- 1 Khushi 197121 29K Feb  9 00:13 D:/MediReact/.git/logs/HEAD
+# -rw-r--r-- 1 Khushi 197121 1010 Feb  9 00:13 D:/MediReact/.git/logs/refs/heads/Diagnosis
+# -rw-r--r-- 1 Khushi 197121 4.4K Feb  6 14:23 D:/MediReact/.git/logs/refs/heads/DocProfile
+# -rw-r--r-- 1 Khushi 197121 188 Feb  9 00:14 D:/MediReact/.git/logs/refs/remotes/origin/Diagnosis
+# -rw-r--r-- 1 Khushi 197121 1.7K Feb  6 14:23 D:/MediReact/.git/logs/refs/remotes/origin/DocProfile
+# -rw-r--r-- 1 Khushi 197121 5.3K Feb  9 00:17 D:/MediReact/.git/logs/refs/remotes/origin/main
+# -r--r--r-- 1 Khushi 197121 367 Feb  9 00:13 D:/MediReact/.git/objects/02/4142e831e189ebe179244f40dff7b94bae8b95
+# -r--r--r-- 1 Khushi 197121 896 Feb  5 22:57 D:/MediReact/.git/objects/03/aeea3d3f37be66a9a740eebdbee3eb0c8d2236
+# -r--r--r-- 1 Khushi 197121 563 Feb  6 13:50 D:/MediReact/.git/objects/04/2e1e881057b6d3c6f227beb05fd630662f8493
+# -r--r--r-- 1 Khushi 197121 1.2K Feb  6 13:26 D:/MediReact/.git/objects/05/a47ff8f68b90c60cae4c950ec5b58ad985ec99
+# -r--r--r-- 1 Khushi 197121 245 Feb  6 13:51 D:/MediReact/.git/objects/05/e344eeed8bc32029590d5ec96e6e81466b3306
+# -r--r--r-- 1 Khushi 197121 370 Feb  6 13:26 D:/MediReact/.git/objects/06/41c7c07dc92ce2b24d6522607d3ec288c900e1
