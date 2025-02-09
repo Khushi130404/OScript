ab6f31298fd04cde1e132803764084f2ae28972a
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-02-09 19:07:15 +0530

    Process_Management

--- Assignment3/Process_Management/pr_1.sh
+++ Assignment3/Process_Management/pr_1.sh
@@ -1,2 +1,9 @@
 ps aux
 kill -9 365 // -9 For Force Kill
+
+# Khushi@KhushiLaptop MINGW64 /d/OScript/Assignment3/Process_Management (Submission)
+# $ bash pr_1.sh
+#       PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
+#      1741    1675    1741      51436  cons0     197609 18:59:24 /usr/bin/bash
+#      1742    1741    1741      19764  cons0     197609 18:59:24 /usr/bin/ps
+#      1675       1    1675      51616  cons0     197609 18:58:36 /usr/bin/bash
--- Assignment3/Process_Management/pr_2.sh
+++ Assignment3/Process_Management/pr_2.sh
@@ -5,3 +5,10 @@ sleep 10 &
 fg %1
 echo "In FG"
 kill %1
+
+# Khushi@KhushiLaptop MINGW64 /d/OScript/Assignment3/Process_Management (Submission)
+# $ bash pr_2.sh
+# In BG
+# [1] 1234
+# In FG
+# [1]+  Terminated   sleep 10
