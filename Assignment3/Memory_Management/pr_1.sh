77ce7f60e65ab3df2aded22db9fb8cbaeb8d2f3c
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-02-09 20:01:37 +0530

    Memory_Management

--- Assignment3/Memory_Management/pr_1.sh
+++ Assignment3/Memory_Management/pr_1.sh
@@ -3,3 +3,18 @@ which free
 echo ""
 echo "Using MemInfo : "
 cat /proc/meminfo
+
+# Khushi@KhushiLaptop MINGW64 /d/OScript/Assignment3/Memory_Management (Submission)
+# $ bash pr_1.sh
+# Using Free : 
+# which: no free in (/c/Users/Khushi/bin:/mingw64/bin:/usr/local/bin:/usr/bin:/bin:/mingw64/bin:/usr/bin:/c/Users/Khushi/bin:/c/Program Files/Python312/Scripts:/c/Program Files/Python312:/c/Program Files/Common Files/Oracle/Java/javapath:/c/Windows/system32:/c/Windows:/c/Windows/System32/Wbem:/c/Windows/System32/WindowsPowerShell/v1.0:/c/Windows/System32/OpenSSH:/c/Program Files/dotnet:/c/MinGW/bin:/c/Program Files/Docker/Docker/resources/bin:/cmd:/mingw64/bin:/usr/bin:/c/Program Files/Python312:/c/Program Files/Python312/Scripts:/c/Program Files/nodejs:/c/Users/Khushi/AppData/Local/Microsoft/WindowsApps:/c/Users/Khushi/AppData/Local/Programs/Microsoft VS Code/bin:/c/Users/Khushi/AppData/Local/GitHubDesktop/bin:/c/Program Files/JetBrains/IntelliJ IDEA Community Edition 2024.1/bin:/c/Program Files/JetBrains/PyCharm Community Edition 2024.2.1/bin:/c/Users/Khushi/AppData/Roaming/npm:/usr/bin/vendor_perl:/usr/bin/core_perl)
+
+# Using MemInfo :
+# MemTotal:       16454352 kB
+# MemFree:         3338000 kB
+# HighTotal:             0 kB
+# HighFree:              0 kB
+# LowTotal:       16454352 kB
+# LowFree:         3338000 kB
+# SwapTotal:      17914348 kB
+# SwapFree:       16446432 kB
