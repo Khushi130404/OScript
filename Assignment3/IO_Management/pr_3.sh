5f1e8ad192ec546dd1ec6c5159a9f922b1277aa4
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-02-09 20:18:19 +0530

    IO_Management

--- Assignment3/IO_Management/pr_3.sh
+++ Assignment3/IO_Management/pr_3.sh
@@ -1,3 +1,9 @@
 # Mount a USB drive and inspect its filesystem.
 
 blkid
+
+# Khushi@KhushiLaptop MINGW64 /d/OScript/Assignment3/IO_Management (Submission)
+# $ bash pr_3.sh
+# /dev/sda1: UUID="1234-ABCD" TYPE="vfat" PARTUUID="abcd5678-01"  
+# /dev/sda2: UUID="abcd-5678-efgh-1234" TYPE="ext4" PARTUUID="abcd5678-02"  
+# /dev/sdb1: UUID="8765-DCBA" TYPE="ntfs" PARTUUID="dcba4321-01"  
